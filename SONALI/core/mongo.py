25d020dc673f1e6666caf37b34930d2bb32f5a1c
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-07-01 03:32:47 +0530

    Update mongo.py

--- SONALI/core/mongo.py
+++ SONALI/core/mongo.py
@@ -1,17 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 from pyrogram import Client
-
 import config
-
 from ..logging import LOGGER
 
 TEMP_MONGODB = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority"
 
-
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐕𝐈𝐏 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
+        "No MONGO_DB_URI Found. Using TEMP DB from VIP MUSIC fallback."
     )
     temp_client = Client(
         "SONALI",
@@ -23,12 +20,19 @@ if config.MONGO_DB_URI is None:
     info = temp_client.get_me()
     username = info.username
     temp_client.stop()
+
     _mongo_async_ = _mongo_client_(TEMP_MONGODB)
     _mongo_sync_ = MongoClient(TEMP_MONGODB)
+
     mongodb = _mongo_async_[username]
     pymongodb = _mongo_sync_[username]
+
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Yukki
-    pymongodb = _mongo_sync_.Yukki
+
+    mongodb = _mongo_async_["Yukki"]
+    pymongodb = _mongo_sync_["Yukki"]
+
+# ✅ Add chatbot collection (required for chatbot on/off status)
+chatbotdb = mongodb["chatbot"]
